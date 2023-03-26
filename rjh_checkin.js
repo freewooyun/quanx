14b23aeee4428052c04cb4ade895e7ab9a1907fa
freewooyun <freewooyun@users.noreply.github.com>
2023-03-26 11:41:14 +0800

    Update rjh_checkin.js

--- rjh_checkin.js
+++ rjh_checkin.js
@@ -1,4 +1,4 @@
-// 设置请求头信息
+// 设置请求头
 const headers = {
   'Connection': 'keep-alive',
   'X-Requested-With': 'XMLHttpRequest',
@@ -9,29 +9,28 @@ const headers = {
   'Content-Length': '0',
   'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 15_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Homeinns-App-iOS-9.17.2',
   'Referer': 'http://idea.homeinns.com/event/check_in/home?client_info=ios&auth_code=2ddfa631c84b526d7b49',
-  'Cookie': 'a012bb6b2123984c_gr_session_id=5494e7a1-7b07-48b0-a092-354472f36b42; a012bb6b2123984c_gr_session_id_5494e7a1-7b07-48b0-a092-354472f36b42=true; gr_user_id=3794fce5-1150-47d1-8791-5eb5e9229700; Hm_lpvt_9c85f54259b6f9e8829573471da9274f=1679797290; Hm_lvt_9c85f54259b6f9e8829573471da9274f=1679797255; _rujia-expand_session=c5b17303440d70670f05193fa96498ea',
-  'X-CSRF-Token': '55xT4It9b+u2QZnMEHMsN34fnc65FqbnS2DG5NJJD+aQRaZP1lpMVxX4S6Mav0jEo3GwHRCEb4ozmyyQ1lTuCw=='
-}
+  'Cookie': 'a012bb6b2123984c_gr_session_id=5494e7a1-7b07-48b0-a092-354472f36b42; a012bb6b2123984c_gr_session_id_5494e7a1-7b07-48b0-a092-354472f36b42=true; gr_user_id=3794fce5-1150-47d1-8791-5eb5e9229700; Hm_lpvt_9c85f54259b6f9e8829573471da9274f=1679797290; Hm_lvt_9c85f54259b6f9e8829573471da9274f=1679797255; _rujia-expand_session=c5b17303440d70670f05193fa96498ea'
+};
 
 // 发送POST请求
-const url = 'http://idea.homeinns.com/event/check_in/home'
-const method = 'POST'
-const body = {}
 const myRequest = {
-  url: url,
-  method: method,
+  url: 'http://idea.homeinns.com/event/check_in/home',
+  method: 'POST',
   headers: headers,
-  body: body
-}
+  body: {}
+};
 
 $task.fetch(myRequest).then(response => {
-  const body = response.body
-  const result = JSON.parse(body)
-  if (result.code === 0) {
-    $notify("如家自动签到", "", "签到成功")
+  // 解析响应数据
+  const result = JSON.parse(response.body);
+  if (result.result_code === 0) {
+    console.log('自动签到成功');
+    $notify('如家酒店', '自动签到成功', result.data.prizes[0].prize_name);
   } else {
-    $notify("如家自动签到", "", "签到失败，请检查配置信息")
+    console.log('自动签到失败：' + result.result);
+    $notify('如家酒店', '自动签到失败', result.result);
   }
 }, reason => {
-  $notify("如家自动签到", "", "签到失败，请检查网络连接")
-})
+  console.log('自动签到失败：' + reason.error);
+  $notify('如家酒店', '自动签到失败', reason.error);
+});
